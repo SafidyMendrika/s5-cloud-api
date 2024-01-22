67034f6febc80e962e39c6e11b6749cb4f2d526e
MirijaMarc <mirijarazafimbelo30@gmail.com>
2024-01-22 11:00:15 +0300

    inti

--- src/main/java/com/cloudapi/security/SecurityConfiguration.java
+++ src/main/java/com/cloudapi/security/SecurityConfiguration.java
@@ -30,18 +30,18 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http
-            .csrf(csrf -> csrf
-                    .disable())
-            .authorizeHttpRequests(requests -> requests
-                    .requestMatchers("/api/auth/**")
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated())
-            .sessionManagement(management -> management
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authenticationProvider(authenticationProvider)
-            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        // http
+        //     .csrf(csrf -> csrf
+        //             .disable())
+        //     .authorizeHttpRequests(requests -> requests
+        //             .requestMatchers("/api/auth/**")
+        //             .permitAll()
+        //             .anyRequest()
+        //             .authenticated())
+        //     .sessionManagement(management -> management
+        //             .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        //     .authenticationProvider(authenticationProvider)
+        //     .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
