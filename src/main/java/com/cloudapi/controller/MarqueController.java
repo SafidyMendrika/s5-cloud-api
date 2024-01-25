6fec759041940a8bc8e100c26de6444fcd0fe322
MirijaMarc <mirijarazafimbelo30@gmail.com>
2024-01-25 22:26:31 +0300

    cors marque

--- src/main/java/com/cloudapi/controller/MarqueController.java
+++ src/main/java/com/cloudapi/controller/MarqueController.java
@@ -3,6 +3,7 @@ package com.cloudapi.controller;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import jakarta.persistence.PersistenceContext;
 
 @RestController
 @RequestMapping("/api/marques")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class MarqueController {
     @PersistenceContext
     private EntityManager entityManager;
