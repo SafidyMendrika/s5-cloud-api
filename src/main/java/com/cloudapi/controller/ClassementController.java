59fdd08ffdd4839740c4aba1cbb5570fc2d3b7c1
Razafimalaza Safidy Mendrika <mendrikarazafimalaza@gmail.com>
2024-01-26 09:19:10 +0300

    imports

--- src/main/java/com/cloudapi/controller/ClassementController.java
+++ src/main/java/com/cloudapi/controller/ClassementController.java
@@ -15,10 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cloudapi.dto.AnnonceDTO;
 import com.cloudapi.json.Response;
-import com.cloudapi.model.Annonce;
-import com.cloudapi.model.Benefice;
 import com.cloudapi.model.ClassementAnnonce;
 
 import jakarta.persistence.EntityManager;
