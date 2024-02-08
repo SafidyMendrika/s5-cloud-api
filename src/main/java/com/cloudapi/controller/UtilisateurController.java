03abddad046202cc00f07b3099a58d3fae8b12b2
Razafimalaza Safidy Mendrika <mendrikarazafimalaza@gmail.com>
2024-02-09 00:40:59 +0300

    notification firebase

--- src/main/java/com/cloudapi/controller/UtilisateurController.java
+++ src/main/java/com/cloudapi/controller/UtilisateurController.java
@@ -66,6 +66,7 @@ public class UtilisateurController {
             rep.success("Login d'un utilisateur", service.authenticate(utilisateurDTO));
             return ResponseEntity.ok(rep);
         } catch (Exception e) {
+            e.printStackTrace();
             rep.error(new Exception("Email ou mot de passe incorrect"));
         }
         return ResponseEntity.ok(rep);
