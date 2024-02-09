222d4a23b26f0cfef485232a77de7ab0cebf8d34
MirijaMarc <mirijarazafimbelo30@gmail.com>
2024-02-09 13:25:18 +0300

    annonce

--- src/main/java/com/cloudapi/CloudapiApplication.java
+++ src/main/java/com/cloudapi/CloudapiApplication.java
@@ -1,5 +1,9 @@
 package com.cloudapi;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +12,7 @@ import org.springframework.core.io.ClassPathResource;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.StorageClient;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 
@@ -15,6 +20,23 @@ import com.google.firebase.messaging.FirebaseMessaging;
 
 @SpringBootApplication
 public class CloudapiApplication {
+
+
+	@Bean
+	StorageClient firebaseStorage() throws Exception{
+		FirebaseApp app = null;
+        File f = new File("firebase/s5-cloud-api-file-firebase-adminsdk-7b445-29e99095c2.json");
+        InputStream serviceAccount = new FileInputStream(f);
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setStorageBucket("s5-cloud-api-file.appspot.com")
+                .build();
+
+        app= FirebaseApp.initializeApp(options, "mirija");
+
+
+        return StorageClient.getInstance(app);
+	}
 
 	@Bean
 	FirebaseMessaging firebaseMessaging() throws Exception{
